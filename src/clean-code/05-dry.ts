63a0f1f91a365ae84c9f079d6aa3afa6e7f9268e
Alejandro Dubon <alejandrodubon88@gmail.com>
2022-05-30 16:41:15 -0600

    [working] single responsability principle

--- src/clean-code/05-dry.ts
+++ src/clean-code/05-dry.ts
@@ -1,23 +1,46 @@
-type Size = '' | 'S' | 'M' | 'L' | 'XL';
+type Size = "" | "S" | "M" | "L" | "XL";
 
 class Product {
     constructor(
-        public name: string = '',
+        public name: string = "",
         public price: number = 0,
-        public size: Size = '',
-        ) {}
+        public size: Size = ""
+    ) {}
+
+    // No Dry
+    // toString() {
+    //     if (this.name.length <= 0) throw new Error('Product name is empty');
+    //     if (this.price <= 0) throw new Error('Product price is not valid');
+    //     if (this.size.length <= 0) throw new Error('Product size is not valid');
+    //     return `${this.name} (${this.price}) ${this.size}`;
+    // }
+
+    isProductReady(): boolean {
+        for (const key in this) {
+            switch (typeof this[key]) {
+                case "string":
+                    if ((<string>(<unknown>this[key])).length <= 0)
+                        throw new Error(`${key} is empty`);
+                    break;
+                case "number":
+                    if (<number>(<unknown>this[key]) <= 0)
+                        throw new Error(`${key} is zero`);
+                    break;
+                default:
+                    throw new Error(`${typeof this[key]} is not supported`);
+            }
+        }
+
+        return true;
+    }
 
     toString() {
-        // No Dry
-        if (this.name.length <= 0) throw new Error('Product name is empty');
-        if (this.price <= 0) throw new Error('Product price is not valid');
-        if (this.size.length <= 0) throw new Error('Product size is not valid');
-
+        if (!this.isProductReady()) throw new Error("Product is not ready");
         return `${this.name} (${this.price}) ${this.size}`;
     }
 }
 
 (() => {
-    const bluePants = new Product("blue large pants", 10, 'L');
+    const bluePants = new Product("blue large pants", 10, "L");
     console.log(bluePants.toString());
-})()
+})();
